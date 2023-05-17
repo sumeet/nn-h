074ea488ee08e6711c937ed6c6fc5d89123b80fb
Sumeet Agarwal <sumeet.a@gmail.com>
2023-05-16 18:08:47 -0700

    it all works!

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,20 @@ impl<'a> Mat<'a> {
         }
     }
 
+    fn at(&self, r: usize, c: usize) -> f32 {
+        self.elements[r * self.cols + c]
+    }
+
+    fn at_mut(&mut self, r: usize, c: usize) -> &mut f32 {
+        &mut self.elements.to_mut()[r * self.cols + c]
+    }
+
+    fn fill(&mut self, val: f32) {
+        for e in self.elements.to_mut() {
+            *e = val;
+        }
+    }
+
     fn copy_from(&mut self, other: &Mat) {
         assert_eq!(self.rows, other.rows);
         assert_eq!(self.cols, other.cols);
@@ -120,6 +134,28 @@ where
         }
     }
 
+    fn zero(&mut self) {
+        for w in &mut self.weights {
+            for e in w.elements.to_mut() {
+                *e = 0.;
+            }
+        }
+        for b in &mut self.biases {
+            for e in b.elements.to_mut() {
+                *e = 0.;
+            }
+        }
+        for a in &mut self.activations {
+            for e in a.elements.to_mut() {
+                *e = 0.;
+            }
+        }
+    }
+
+    fn input(&self) -> &'a Mat {
+        &self.activations[0]
+    }
+
     fn input_mut(&mut self) -> &'a mut Mat {
         &mut self.activations[0]
     }
@@ -185,6 +221,96 @@ where
             next_activation.sigmoid();
         }
     }
+
+    fn get_output_for(&'a mut self, input: &Mat) -> &'a Mat {
+        let self_ptr: *mut _ = self;
+        assert_eq!(input.cols, self.input().cols);
+        let this = unsafe { &mut *self_ptr };
+        this.input_mut().copy_from(input);
+        let this = unsafe { &mut *self_ptr };
+        this.forward();
+        self.output()
+    }
+
+    fn backprop(&'a mut self, g: &mut NN<LAYERS>, input: &Mat, output: &Mat) {
+        assert_eq!(input.rows, output.rows);
+        let n = input.rows;
+        assert_eq!(output.cols, self.output().cols);
+
+        g.zero();
+
+        // i - current sample
+        // l - current layer
+        // j - current activation
+        // k - previous activation
+        let self_ptr: *mut _ = self;
+
+        for i in 0..n {
+            {
+                let this = unsafe { &mut *self_ptr };
+                this.input_mut().copy_from(&input.row(i));
+            }
+            self.forward();
+
+            let g_ptr: *mut _ = g;
+            for j in 0..LAYERS {
+                let g_mut = unsafe { &mut *g_ptr };
+                g_mut.activations[j].fill(0.);
+            }
+
+            for j in 0..self.output().cols {
+                let d = self.output().at(0, j) - output.at(i, j);
+                let g_mut = unsafe { &mut *g_ptr };
+                *g_mut.output_mut().at_mut(0, j) = d;
+            }
+
+            for l in (1..=LAYERS).rev() {
+                for j in 0..self.activations[l].cols {
+                    let a = self.activations[l].at(0, j);
+                    let da = g.activations[l].at(0, j);
+                    *g.biases[l - 1].at_mut(0, j) += 2. * da * a * (1. - a);
+                    for k in 0..self.activations[l - 1].cols {
+                        // j - weight matrix col
+                        // k - weight matrix row
+                        // pa is the activation of the previous layer
+                        let pa = self.activations[l - 1].at(0, k);
+                        let w = self.weights[l - 1].at(k, j);
+                        *g.weights[l - 1].at_mut(k, j) += 2. * da * a * (1. - a) * pa;
+                        *g.activations[l - 1].at_mut(0, k) += 2. * da * a * (1. - a) * w;
+                    }
+                }
+            }
+        }
+
+        for i in 0..LAYERS {
+            for j in 0..g.weights[i].rows {
+                for k in 0..g.weights[i].cols {
+                    *g.weights[i].at_mut(j, k) /= n as f32;
+                }
+            }
+            for j in 0..g.biases[i].rows {
+                for k in 0..g.biases[i].cols {
+                    *g.biases[i].at_mut(j, k) /= n as f32;
+                }
+            }
+        }
+    }
+
+    fn learn(&mut self, g: &NN<LAYERS>, rate: f32) {
+        for i in 0..LAYERS {
+            for j in 0..self.weights[i].rows {
+                for k in 0..self.weights[i].cols {
+                    *self.weights[i].at_mut(j, k) -= rate * g.weights[i].at(j, k);
+                }
+            }
+
+            for j in 0..self.biases[i].rows {
+                for k in 0..self.biases[i].cols {
+                    *self.biases[i].at_mut(j, k) -= rate * g.biases[i].at(j, k);
+                }
+            }
+        }
+    }
 }
 
 fn main() {
@@ -212,8 +338,48 @@ fn main() {
     };
 
     let mut nn = NN::<2>::new([2, 2, 1]);
+    let nn_ptr: *mut _ = &mut nn;
+    train(&mut nn, &training_input, &training_output);
+    let nn_mut = unsafe { &mut *nn_ptr };
+    let new_cost = nn_mut.cost(&training_input, &training_output);
+    dbg!(new_cost);
+
+    // test the neural net
+    for x in 0..=1 {
+        for y in 0..=1 {
+            let nn_mut = unsafe { &mut *nn_ptr };
+            let input = Mat {
+                rows: 1,
+                cols: 2,
+                elements: vec![x as f32, y as f32].into(),
+            };
+            let output = nn_mut.get_output_for(&input);
+            let output = output.at(0, 0);
+            println!("{x} ^ {y} = {} ({output})", (output > 0.5) as u8);
+        }
+    }
+}
+
+fn train<'a, const LAYERS: usize>(
+    mut nn: &'a mut NN<'a, LAYERS>,
+    training_input: &'a Mat,
+    training_output: &'a Mat,
+) where
+    [(); LAYERS + 1]: Sized,
+{
+    let nn_ptr: *mut _ = &mut nn;
+
     let mut g = nn.clone();
     nn.randomize(0., 1.);
-    dbg!(&nn);
-    dbg!(nn.cost(&training_input, &training_output));
+
+    let orig_cost = nn.cost(&training_input, &training_output);
+    dbg!(orig_cost);
+
+    let learn_rate = 1.;
+    for _ in 0..5000 {
+        let nn_mut = unsafe { &mut *nn_ptr };
+        nn_mut.backprop(&mut g, &training_input, &training_output);
+        let nn_mut = unsafe { &mut *nn_ptr };
+        nn_mut.learn(&g, learn_rate);
+    }
 }
